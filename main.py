66d80e31beb22617ae831e5ada85daaeed45c35b
David Dempsey <david.dempsey@canterbury.ac.nz>
2023-03-06 16:21:42 +1300

    add some post processing to measure forecaster performance

--- main.py
+++ main.py
@@ -2,13 +2,20 @@
 from datetime import timedelta
 from puia.tests import run_tests
 from puia.model import ForecastModel,CombinedModel,MultiDataForecastModel
-from puia.utilities import datetimeify
+from puia.data import SeismicData, GeneralData
+from puia.utilities import datetimeify, load_dataframe
+from glob import glob
+import os
+import pandas as pd
+import numpy as np
 
 _MONTH = timedelta(days=365.25/12)
 DATA_DIR=r'U:\Research\EruptionForecasting\eruptions\data'
 # FEAT_DIR=r'U:\Research\EruptionForecasting\eruptions\features'
 DATA_DIR=r'C:\Users\dde62\code\alberto\EruptionForecasting\data'
 FEAT_DIR=r'C:\Users\dde62\code\alberto\EruptionForecasting\features'
+DATA_DIR=r'/media/eruption_forecasting/eruptions/data'
+FEAT_DIR=r'/media/eruption_forecasting/eruptions/features'
 TI = datetimeify('2011-01-03')
 TF = datetimeify('2019-12-31')
 
@@ -90,6 +97,76 @@ def discriminability(root, data_streams, Ncl, eruption=None):
     # forecast over whole dataset
     fm.hires_forecast(TI, TF, recalculate=False, n_jobs=n_jobs, root=r'{:s}_hires'.format(fm.root), threshold=1.0)    
 
+def get_forecast(root, assumeUpdating=False):
+    from glob import glob
+    d = r'C:\Users\dde62\code\puia\forecasts'
+    # Recursion L1
+    # read in data for eruption
+    if '_e' in root:
+        fls = glob(d+f'\{root}_hires\consensus*.pkl')        
+        ys = []
+        for fl in fls:
+            ys.append(load_dataframe(fl))
+        y0 = pd.concat(ys).sort_index()
+        return y0[(y0.index>TI)&(y0.index<TF)]
+
+    # Recursion L0
+    # read in data for all eruptions
+    y0=get_forecast(f'{root}_e0')
+    y0s=[get_forecast(f'{root}_e{i+1:d}') for i in range(5)]
+    if assumeUpdating:
+        # Special case: OCT 2013 eruption sequence
+        # throws out model results after the first eruption, in which case subsequent
+        # eruptions will be predicted assuming training on first in squence
+        te=datetimeify('2013-10-03 12:35:00')
+        y0s[2] = y0s[2][(y0s[2].index<te)]
+    for i,yi in enumerate(y0s):
+        inds=(y0.index>yi.index[0])&(y0.index<yi.index[-1])        
+        y0.loc[inds,'consensus']=np.interp(y0.index[inds], xp=yi.index, fp=yi['consensus'])
+    
+    # y0=pd.concat(y0s)
+    
+    # Allow eruption out-of-sample simulations (i>0) to overwrite non-eruption (i=0)
+    # y0 = y0[~y0.index.duplicated(keep='last')].sort_index()
+    return y0[(y0.index>TI)&(y0.index<TF)]
+
+def performance(root):
+    y=get_forecast(f'{root}', assumeUpdating=True)
+    if 'reference' in root:
+        td=SeismicData(station='WIZ', data_dir=DATA_DIR)
+        rsam=td.df['rsamF']
+    elif 'template' in root:
+        td=GeneralData(station='WIZ', name='dsarTemplate', data_dir=DATA_DIR)
+        rsam=td.df['2019']
+    elif 'physics' in root:
+        td=GeneralData(station='WIZ', name='inversion', data_dir=DATA_DIR)
+        rsam=td.df['Qm']
+    tes=list(td.tes)[:2]+[datetimeify(te) for te in ['2013-10-08 02:05:00','2013-10-11 07:09:00']]+td.tes[2:]
+
+    from matplotlib import pyplot as plt
+    f,axs=plt.subplots(5,1,figsize=(8,8))
+    for te,ax in zip(td.tes, axs):
+        ti=te-_MONTH/6
+        tf=te+_MONTH/15
+        if td.tes.index(te)==2:
+            tf=te+_MONTH/2.
+        inds=(rsam.index>ti)&(rsam.index<tf)
+        ax.plot(rsam.index[inds], rsam[inds].values,'k-',lw=0.5)
+        ax_=ax.twinx()
+        inds=(y.index>ti)&(y.index<tf)
+        ax_.plot(y.index[inds], y[inds].values,'c-',lw=0.5)
+        ax.set_title(f'{te.strftime("%b %Y")}')
+
+        for te in tes:
+            if te>ti and te<tf:
+                ax.axvline(te, color='r',linestyle='--')
+        ax_.set_ylim(-0.1,1.1)
+        ax.set_xlim([ti,tf])
+        ax_.axhline(1, color='k',alpha=0.5,linewidth=0.5)
+    plt.tight_layout()
+    plt.savefig(f'{root}_forecasts.png',dpi=400)
+
+
 def run_models(root, data_streams, Ncl=100):
     # assess reliability by cross validation on five eruptions
     for eruption in range(1,6):
@@ -97,6 +174,9 @@ def run_models(root, data_streams, Ncl=100):
         
     # assess discriminability by high-resoultion simulation across dataset
     discriminability(root, data_streams, Ncl)
+
+    # summarise forecast performance
+    #performance(root)
 
 def test_multi_data_load():
     # invocation types
